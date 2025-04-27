eba666847434b9c01953ce1e1c280ad0edb7cc77
Your Name <you@example.com>
2025-04-26 21:21:50 -0400

    fixing landing page

--- src/types/pdf.d.ts
+++ src/types/pdf.d.ts
@@ -2,7 +2,7 @@ declare module 'pdfjs-dist/build/pdf.worker.entry' {
   const content: string;
   export default content;
 }
-
+ 
 declare module 'pdfjs-dist/webpack' {
   export * from 'pdfjs-dist';
 } 
